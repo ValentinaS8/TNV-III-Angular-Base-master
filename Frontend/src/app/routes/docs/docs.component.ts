652c7ad7c76da2c6895083be88c7999383956135
Gabriel <piogabriel.rocca@gmail.com>
2021-03-14 18:25:45 +0100

    Documentazione aggiornata

--- Frontend/src/app/routes/docs/docs.component.ts
+++ Frontend/src/app/routes/docs/docs.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./docs.component.css']
 })
 export class DocsComponent implements OnInit {
-
+  europe = ["Austria", "Belgio", "Cipro", "Croazia", "Danimarca", "Estonia",
+    "Finlandia", "Francia", "Germania", "Grecia", "Irlanda", "Italia", "Lettonia", "Lituania", "Lussemburgo",
+    "Malta", "Paesi Bassi", "Polonia", "Portogallo", "Regno Unito", "Repubblica Ceca", "Romania",
+    "Slovacchia", "Slovenia", "Spagna", "Svezia", "Svizzera", "Ungheria"];
   constructor() { }
 
   ngOnInit(): void {
